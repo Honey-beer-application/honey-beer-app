24ca2b5189d1f8787e30903da184374b9f2cb46b
Stefan Veljkovic <stef.fac150@gmail.com>
2025-05-22 23:27:03 +0200

    Implemented SonarLint suggestions

--- src/app/Data/Controllers/CompanyController.ts
+++ src/app/Data/Controllers/CompanyController.ts
@@ -1,21 +1,20 @@
-import { Observable } from "rxjs";
+import { Observable, BehaviorSubject } from "rxjs";
 import Company from "../Classes/Company";
 import {Injectable} from '@angular/core'
 import CompanyService from "../Services/Company.service";
 import ICompany from "../Interfaces/ICompany";
-import {BehaviorSubject} from "rxjs"
 
 @Injectable({providedIn:"root"})
 export default class CompanyController{
-    private _registeredCompany:ICompany = new Company().setPIB(10000001n).setEmail("kompanija1@gmail.com").setName("kompanija1").setPassword("kompanija1");
-    private _companyBehaviour:BehaviorSubject<ICompany> = new BehaviorSubject<ICompany>(this._registeredCompany);
+    private readonly _registeredCompany:ICompany = new Company().setPIB(10000001n).setEmail("kompanija1@gmail.com").setName("kompanija1").setPassword("kompanija1");
+    private readonly _companyBehaviour:BehaviorSubject<ICompany> = new BehaviorSubject<ICompany>(this._registeredCompany);
     public get companyObservable(): Observable<ICompany> {
         return this._companyBehaviour.asObservable();
     }
     public setCompany(company:ICompany){
         this._companyBehaviour.next(company);
     }
-    constructor(private companyService:CompanyService){
+    constructor(private readonly companyService:CompanyService){
     }
 
     public createCompany(PIB:bigint,name:string,email:string,password:string):Observable<ICompany>{
--- src/app/Data/Controllers/CustomerConstroller.ts
+++ src/app/Data/Controllers/CustomerConstroller.ts
@@ -1,33 +1,16 @@
-import { Observable } from "rxjs";
+import { Observable, BehaviorSubject } from "rxjs";
 import Customer from "../Classes/Customer";
 import ICustomer from "../Interfaces/ICustomer";
 import { CustomerService } from "../Services/Customer.service";
-import {Injectable} from '@angular/core'
-import PersonalEmail from "../Classes/PersonalEmail";
-import { BehaviorSubject } from "rxjs";
+import {Injectable} from '@angular/core';
 @Injectable({providedIn:"root"})
 export default class CustomerController{
 
-    // private _registeredCustomer: ICustomer;
-
     public registeredCustomer:BehaviorSubject<ICustomer>;
-    // public get registeredCustomer(): ICustomer {
-    //     return this._registeredCustomer;
-    // }
-    // public set registeredCustomer(value: ICustomer) {
-    //     this._registeredCustomer = value;
-    // }
 
-    constructor(private customerService:CustomerService){
+    constructor(private readonly customerService:CustomerService){
         let customer:ICustomer = new Customer();
-        // customer.customerId=13n;
-        // customer.email="customer10@gmail.com";
-        // customer.password="customer10";
-        // customer.username="customer10";
-        // customer.personalEmailInstance = new PersonalEmail();
-        // customer.personalEmailInstance.email="customer10@gmail.com";
         this.registeredCustomer = new BehaviorSubject(customer);
-        //this.registeredCustomer = new BehaviorSubject(<ICustomer>(new Customer()));
     }
     public createCustomer(username:string,email:string,password:string):Observable<ICustomer>{
         
