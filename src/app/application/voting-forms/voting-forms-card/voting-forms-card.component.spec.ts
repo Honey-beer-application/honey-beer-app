7bb1d6e98a58cf8fadd703fc29f6af4325df2e77
Stefan Veljkovic <stef.fac150@gmail.com>
2025-05-30 03:11:01 +0200

    Tested and improved VotingFormsComponent

--- src/app/application/voting-forms/voting-forms-card/voting-forms-card.component.spec.ts
+++ src/app/application/voting-forms/voting-forms-card/voting-forms-card.component.spec.ts
@@ -2,6 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VotingFormsCardComponent } from './voting-forms-card.component';
 import { provideHttpClient } from '@angular/common/http';
+import { Event } from 'src/app/Data/Classes/Event';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SurveyComponent } from '../../surveys/survey/survey.component';
 
 describe('VotingFormsCardComponent', () => {
   let component: VotingFormsCardComponent;
@@ -10,7 +13,8 @@ describe('VotingFormsCardComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [VotingFormsCardComponent],
-      providers: [provideHttpClient()]
+      providers: [provideHttpClient()],
+      imports: [RouterTestingModule.withRoutes([{path:'app/surveys/1', component:SurveyComponent}])]
     });
     fixture = TestBed.createComponent(VotingFormsCardComponent);
     component = fixture.componentInstance;
@@ -20,4 +24,7 @@ describe('VotingFormsCardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should throw no exception on reiggering redirectToSurvey',()=>{
+    expect(()=>component.redirectToSurvey(new Event({eventId:1n}))).not.toThrow();
+  })
 });
--- src/app/application/voting-forms/voting-forms.component.spec.ts
+++ src/app/application/voting-forms/voting-forms.component.spec.ts
@@ -2,16 +2,26 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VotingFormsComponent } from './voting-forms.component';
 import { provideHttpClient } from '@angular/common/http';
+import { IEvent } from 'src/app/Data/Interfaces/IEvent';
+import { Event } from 'src/app/Data/Classes/Event';
+import { EventController } from 'src/app/Data/Controllers/EventController';
+import { of } from 'rxjs';
+import { VotingFormsCardComponent } from './voting-forms-card/voting-forms-card.component';
 
 describe('VotingFormsComponent', () => {
   let component: VotingFormsComponent;
   let fixture: ComponentFixture<VotingFormsComponent>;
+  let eventsToBeReturned: IEvent[] = [new Event()];
+  let eventControllerSpyObj: jasmine.SpyObj<EventController>;
 
   beforeEach(() => {
+    eventControllerSpyObj =jasmine.createSpyObj('EventController',['loadAllEvents']);
     TestBed.configureTestingModule({
-      declarations: [VotingFormsComponent],
-      providers: [provideHttpClient()]
+      declarations: [VotingFormsComponent, VotingFormsCardComponent],
+      providers: [provideHttpClient(), {provide: EventController, useValue: eventControllerSpyObj}]
     });
+    eventControllerSpyObj = TestBed.inject(EventController) as jasmine.SpyObj<EventController>;
+    eventControllerSpyObj.loadAllEvents.and.returnValue(of(eventsToBeReturned));
     fixture = TestBed.createComponent(VotingFormsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +30,10 @@ describe('VotingFormsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should return event list',()=>{
+    let comp : VotingFormsComponent = new VotingFormsComponent(eventControllerSpyObj);
+    expect(comp.surveys).toEqual(eventsToBeReturned);
+
+  })
 });
