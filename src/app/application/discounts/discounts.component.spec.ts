052345999ecb3feac1ac1df63a973972787f1219
Stefan Veljkovic <stef.fac150@gmail.com>
2025-05-30 03:05:37 +0200

    Tested DiscountsComponent

--- src/app/application/discounts/discounts.component.spec.ts
+++ src/app/application/discounts/discounts.component.spec.ts
@@ -2,16 +2,25 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DiscountsComponent } from './discounts.component';
 import { provideHttpClient } from '@angular/common/http';
+import { DiscountController } from 'src/app/Data/Controllers/DiscountController';
+import { of, throwError } from 'rxjs';
+import { Discount } from 'src/app/Data/Classes/Discount';
+import { DiscountCardComponent } from './discount-card/discount-card.component';
+import CustomError from 'src/app/Data/Classes/CustomError';
 
 describe('DiscountsComponent', () => {
   let component: DiscountsComponent;
   let fixture: ComponentFixture<DiscountsComponent>;
+  let discountControllerSpyObj: jasmine.SpyObj<DiscountController>
 
   beforeEach(() => {
+    discountControllerSpyObj = jasmine.createSpyObj('DiscountController',['loadAllDiscounts']);
     TestBed.configureTestingModule({
-      declarations: [DiscountsComponent],
-      providers: [provideHttpClient()]
+      declarations: [DiscountsComponent,DiscountCardComponent],
+      providers: [provideHttpClient(),{provide: DiscountController, useValue: discountControllerSpyObj}]
     });
+    discountControllerSpyObj = TestBed.inject(DiscountController) as jasmine.SpyObj<DiscountController>;
+    discountControllerSpyObj.loadAllDiscounts.and.returnValue(of([new Discount(), new Discount()]));
     fixture = TestBed.createComponent(DiscountsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +29,11 @@ describe('DiscountsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should not collect discounts',()=>{
+    let exception: CustomError = new CustomError('Discounts have not been loaded');
+    discountControllerSpyObj.loadAllDiscounts.and.returnValue(throwError(()=>exception));
+    spyOn(window,'alert');
+    expect(()=>new DiscountsComponent(discountControllerSpyObj)).not.toThrow();
+    expect(window.alert).toHaveBeenCalledWith(exception.message);
+  })
 });
