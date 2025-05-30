60e882d5c81a1c19b84e235d3c59384da2b0b119
Stefan Veljkovic <stef.fac150@gmail.com>
2025-05-30 03:05:15 +0200

    Tested CreateReservationComponent

--- src/app/application/create-reservation/product-card/product-card.component.spec.ts
+++ src/app/application/create-reservation/product-card/product-card.component.spec.ts
@@ -6,15 +6,20 @@ import { provideHttpClient } from '@angular/common/http';
 import { ProductController } from 'src/app/Data/Controllers/ProductController';
 import { ReservationController } from 'src/app/Data/Controllers/ReservationController';
 import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ProductComponent } from '../../products/product/product.component';
+import { Product } from 'src/app/Data/Classes/Product';
+import { NewReservationComponent } from '../new-reservation/new-reservation.component';
 
 describe('ProductCardComponent', () => {
   let component: ProductCardComponent;
   let fixture: ComponentFixture<ProductCardComponent>;
+  let productId:bigint = 1n;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ProductCardComponent],
-      imports: [RouterModule],
+      imports: [RouterModule, RouterTestingModule.withRoutes([{path:'app/products/'+productId, component: ProductComponent},{path:'app/create-reservation/'+productId+"/new-reservation", component: NewReservationComponent}])],
       providers: [provideHttpClientTesting(), provideHttpClient()]
     });
     fixture = TestBed.createComponent(ProductCardComponent);
@@ -27,4 +32,12 @@ describe('ProductCardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should redirect to product page without error',()=>{
+    expect(()=>component.redirectToProduct(new Product({productId:productId}))).not.toThrow();
+  });
+
+  it('should redirect to new reservation page without error',()=>{
+    expect(()=>component.redirectToNewReservation(new Product({productId:productId}))).not.toThrow();
+  })
 });
